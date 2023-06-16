f9425551bea8fad81b33b2b257ea9f1b6a4dd78b
Jacopo Cardinaux <jcardina@e3r6p4.42roma.it>
2023-06-16 18:47:27 +0200

    da implementare il caso del 5 ed il rotate finale

--- algoritm/best_move.c
+++ algoritm/best_move.c
@@ -6,35 +6,11 @@
 /*   By: jcardina <jcardina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 12:04:34 by jcardina          #+#    #+#             */
-/*   Updated: 2023/06/16 18:11:48 by jcardina         ###   ########.fr       */
+/*   Updated: 2023/06/16 18:43:57 by jcardina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
-
-// void	read_inst(t_listx **stack_a, t_listx **stack_b, t_inst *inst)
-// {
-// 	//questo va bene nel caso tiger
-// 	while (inst ->a_index > 0 && inst ->b_index > 0)
-// 	{
-// 		rr(stack_a, stack_b);
-// 		inst ->a_index--;
-// 		inst ->b_index--;
-// 	}
-// 	while (inst ->a_index > 0)
-// 	{
-// 		ra(stack_a);
-// 		inst ->a_index--;
-// 	}
-// 	while (inst ->b_index > 0)
-// 	{
-// 		rb(stack_b);
-// 		inst ->b_index--;
-// 	}
-// 	pa(stack_a, stack_b);
-// 	inst_init(inst);
-// 	return ;
-// }
 
 void	algoritm(t_listx **stack_a, t_listx **stack_b, t_inst	*inst)
 {
@@ -42,7 +18,6 @@ void	algoritm(t_listx **stack_a, t_listx **stack_b, t_inst	*inst)
 	{
 		best_move(stack_a, stack_b, inst);
 		inst_reader(stack_a, stack_b, inst);
-		//read_inst(stack_a, stack_b, inst);
 	}
 }
 
@@ -59,7 +34,6 @@ void	best_move(t_listx **stack_a, t_listx **stack_b, t_inst	*inst)
 	{
 		count = spot_finder(stack_a, &tmp);
 		moves = moves_counter(count, tmp ->index, stack_a, stack_b);
-		//if ((count + tmp ->index) < (inst ->a_index + inst ->b_index))
 		if (moves < moves_mem)
 		{
 			inst ->a_index = count;
--- algoritm/inst_reader.c
+++ algoritm/inst_reader.c
@@ -6,7 +6,7 @@
 /*   By: jcardina <jcardina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 18:59:32 by jcardina          #+#    #+#             */
-/*   Updated: 2023/06/16 18:31:41 by jcardina         ###   ########.fr       */
+/*   Updated: 2023/06/16 18:43:09 by jcardina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,53 +65,14 @@ void	cobra(t_listx **stack_a, t_listx **stack_b, t_inst *inst)
 	pa(stack_a, stack_b);
 	inst_init(inst);
 	return ;
-	// while (inst ->a_index > 0 && inst ->b_index > 0)
-	// {
-	// 	rr(stack_a, stack_b);
-	// 	inst ->a_index--;
-	// 	inst ->b_index--;
-	// }
-	// while (inst ->a_index > 0)
-	// {
-	// 	ra(stack_a);
-	// 	inst ->a_index--;
-	// }
-	// while (inst ->b_index > 0)
-	// {
-	// 	rb(stack_b);
-	// 	inst ->b_index--;
-	// }
-	// pa(stack_a, stack_b);
-	// inst_init(inst);
-	// return ;
 }
 
 void	labrador(t_listx **stack_a, t_listx **stack_b, t_inst *inst)
 {
-	// while (inst ->a_index <= ps_lstsize(*stack_a))
-	// {
-	// 	rra(stack_a);
-	// 	inst ->a_index++;
-	// }
-	// while (inst ->b_index > 0)
-	// {
-	// 	rb(stack_b);
-	// 	inst ->b_index--;
-	// }
-	// pa(stack_a, stack_b);
-	// inst_init(inst);
-	// return ;
-
-	while (inst ->a_index > 0 && inst ->b_index > 0)
+	while (inst ->a_index < ps_lstsize(*stack_a))
 	{
-		rr(stack_a, stack_b);
-		inst ->a_index--;
-		inst ->b_index--;
-	}
-	while (inst ->a_index > 0)
-	{
-		ra(stack_a);
-		inst ->a_index--;
+		rra(stack_a);
+		inst ->a_index++;
 	}
 	while (inst ->b_index > 0)
 	{
@@ -125,41 +86,21 @@ void	labrador(t_listx **stack_a, t_listx **stack_b, t_inst *inst)
 
 void	kyte(t_listx **stack_a, t_listx **stack_b, t_inst *inst)
 {
-	// while (inst ->a_index <= ps_lstsize(*stack_a) && inst ->b_index < ps_lstsize(*stack_b))
-	// {
-	// 	rrr(stack_a, stack_b);
-	// 	inst ->a_index++;
-	// 	inst ->b_index++;
-	// }
-	// while (inst ->a_index <= ps_lstsize(*stack_a))
-	// {
-	// 	rra(stack_a);
-	// 	inst ->a_index++;
-	// }
-	// while (inst ->b_index < ps_lstsize(*stack_b))
-	// {
-	// 	rrb(stack_b);
-	// 	inst ->b_index++;
-	// }
-	// pa(stack_a, stack_b);
-	// inst_init(inst);
-	// return ;
-
-	while (inst ->a_index > 0 && inst ->b_index > 0)
+	while (inst ->a_index < ps_lstsize(*stack_a) && inst ->b_index < ps_lstsize(*stack_b))
 	{
-		rr(stack_a, stack_b);
-		inst ->a_index--;
-		inst ->b_index--;
+		rrr(stack_a, stack_b);
+		inst ->a_index++;
+		inst ->b_index++;
 	}
-	while (inst ->a_index > 0)
+	while (inst ->a_index < ps_lstsize(*stack_a))
 	{
-		ra(stack_a);
-		inst ->a_index--;
+		rra(stack_a);
+		inst ->a_index++;
 	}
-	while (inst ->b_index > 0)
+	while (inst ->b_index < ps_lstsize(*stack_b))
 	{
-		rb(stack_b);
-		inst ->b_index--;
+		rrb(stack_b);
+		inst ->b_index++;
 	}
 	pa(stack_a, stack_b);
 	inst_init(inst);
